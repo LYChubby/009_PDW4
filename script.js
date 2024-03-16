887cba66c70f103a19ad7d93d005c15785cc597a
LYChubby <f4uzi345@gmail.com>
2024-03-16 18:29:40 +0700

    Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function habis () {
+  alert("Maaf Menu Ini Telah Habis")
+}
+
+function order() {
+  alert("Terimakasih Sudah Pesan")
+}
+
+function stock() {
+  alert("Menu Ini Belum Tersedia")
+}
+
+function besok() {
+  alert("Silakan Pesan Besok Lagi")
+}
